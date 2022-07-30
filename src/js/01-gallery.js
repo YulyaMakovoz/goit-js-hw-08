beac176ea763b5175069d3a389f1e01cef55b361
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-07-30 18:46:12 +0300

    Добавляет задачу №1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. Используй готовый код из первого задания.
+const gallery = document.querySelector('.gallery');
+const makeGallery = galleryItems.map(({ preview, original, description }) => {
+    return `<li> <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a> </li>`}).join(" ");
+
+
+gallery.insertAdjacentHTML("beforeend", makeGallery);
+
+// Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery. Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+var lightbox = new SimpleLightbox('.gallery a', { 
+    // Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt. Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
+    captions: true,
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+ });
+
+
+// Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. Необходимо добавить ссылки на два файла: simple-lightbox.min.js и simple-lightbox.min.css.
+// в HTML
