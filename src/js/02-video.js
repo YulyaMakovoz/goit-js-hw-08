6006795403f425a564d9a9ed316dc163329d1744
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-07-30 22:36:02 +0300

    Добавляет вторую задачу

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const throttle = require('lodash.throttle')
+
+
+const stopTime = function(data) {
+    const time = data.seconds;
+    localStorage.setItem("videoplayer-current-time", time);
+    console.log(time);
+};
+
+player.on('timeupdate',throttle(stopTime, 1000));
+
+
+const saveTime = localStorage.getItem("videoplayer-current-time");
+
+if (saveTime) {
+        player.setCurrentTime(saveTime);
+        console.log('save time', saveTime);
+}
