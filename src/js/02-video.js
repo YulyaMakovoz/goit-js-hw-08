bef192421ef601b92a8da4c6b14ac74272b680cb
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-08-01 21:47:21 +0300

    Добавляет 3 задачу

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,9 @@
 import Player from '@vimeo/player';
-import throttle = require("lodash.throttle");
+
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const throttle = require('lodash.throttle')
+const throttle = require('lodash.throttle');
 
 
 const stopTime = function(data) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,31 @@
 
-// При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. В противном случае поля должны быть пустыми.
 
-import throttle = require("lodash.throttle");
 
+throttle = require("lodash.throttle");
 
 
 const inputEl = document.querySelector('input');
 const messageEl = document.querySelector('textarea');
 const formEl = document.querySelector('.feedback-form');
-// const formData = {};
+const formData = {};
+
 
 // Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
 inputEl.addEventListener('input', throttle(onInput, 500));
 messageEl.addEventListener('input', throttle(onInput, 500));
-
+// inputEl.addEventListener('input',onInput);
+// messageEl.addEventListener('input',onInput);
 formEl.addEventListener('submit', onSubmit);
+fillForm();
+
 
 // Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message, в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".
 function onInput(event) {
-    const mail = inputEl.value;
-    const message = messageEl.value;
-    const formData = { mail, message };
+    // const mail = inputEl.value;
+    // const message = messageEl.value;
+    // const formData = { mail, message };
+
+    formData[event.target.name] = event.target.value;
 
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
     
@@ -30,10 +35,28 @@ function onInput(event) {
 function onSubmit(event) {
     event.preventDefault();
 
-    const savedData = localStorage.getItem('feedback-form-state');
-    console.log(savedData);
+    formData.email = inputEl.value;
+    formData.message = messageEl.value;
 
-    localStorage.removeItem('feedback-form-state');
+    // const savedData = localStorage.getItem('feedback-form-state');
+    console.log(formData);
     formEl.reset();
+    localStorage.removeItem('feedback-form-state');
     
+    // event.currentTarget.reset();
 }
+    
+// При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. В противном случае поля должны быть пустыми.
+function fillForm() {
+    const savedData = localStorage.getItem('feedback-form-state');
+    const formObject = JSON.parse(savedData);
+
+    if('feedback-form-state') {
+        inputEl.value = formObject.email;
+        messageEl.value = formObject.message;
+        // const savedEmail = formObject.email;
+        // inputEl.value = savedEmail;
+        // const savedMessage = formObject.message;
+        // messageEl.value = savedMessage;
+    };
+};
