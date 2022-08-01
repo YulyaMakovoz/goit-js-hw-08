a21ab60649e386d87e071aceced1275257119c93
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-08-01 22:03:08 +0300

    Исправляет задачу 3

--- dist/03-feedback.5440b4f4.js
+++ dist/03-feedback.5440b4f4.js
@@ -511,15 +511,10 @@ const formData = {};
 // Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
 inputEl.addEventListener("input", throttle(onInput, 500));
 messageEl.addEventListener("input", throttle(onInput, 500));
-// inputEl.addEventListener('input',onInput);
-// messageEl.addEventListener('input',onInput);
 formEl.addEventListener("submit", onSubmit);
 fillForm();
 // Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message, в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".
 function onInput(event) {
-    // const mail = inputEl.value;
-    // const message = messageEl.value;
-    // const formData = { mail, message };
     formData[event.target.name] = event.target.value;
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 }
@@ -532,14 +527,15 @@ function onSubmit(event) {
     console.log(formData);
     formEl.reset();
     localStorage.removeItem("feedback-form-state");
-// event.currentTarget.reset();
 }
 // При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. В противном случае поля должны быть пустыми.
 function fillForm() {
     const savedData = localStorage.getItem("feedback-form-state");
     const formObject = JSON.parse(savedData);
-    inputEl.value = formObject.email;
-    messageEl.value = formObject.message;
+    if (formObject) {
+        inputEl.value = formObject.email;
+        messageEl.value = formObject.message;
+    }
 }
 
 },{"lodash.throttle":"bGJVT"}],"bGJVT":[function(require,module,exports) {
